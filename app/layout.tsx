1f4d1501f696ce3d268d6906e8a56b7967fa3de9
yannickrocks <yannickmermet@hotmail.com>
2023-08-20 16:25:15 +0100

    feat: check auth state

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import NavMenu from '@/app/NavMenu';
 import './globals.css';
 import { Open_Sans } from 'next/font/google';
 import Link from 'next/link';
+import AuthProvider from './AuthProvider';
 // import AuthProvider from './AuthProvider';
 
 const myFont = Open_Sans({ weight: '400', subsets: ['latin'] });
@@ -17,41 +18,43 @@ interface Props {
 
 export default function RootLayout({ children }: Props) {
   return (
-    // <AuthProvider>
-    <html lang='en'>
-      <body className={myFont.className}>
-        <div className='container'>
-          <NavMenu />
-          <main>{children}</main>
+    <AuthProvider>
+      <html lang='en'>
+        <body className={myFont.className}>
+          <div className='container'>
+            <NavMenu />
+            <main>{children}</main>
 
-          <footer>
-            <p>
-              Created for the{' '}
-              <Link href='https://fireship.io'>
-                Fireship Next.js 13 Full Course
-              </Link>
-            </p>
-            <ul>
-              <li>
-                <Link href={'/about'}>About</Link>
-              </li>{' '}
-              |
-              <li>
-                <Link href={'https://www.youtube.com/@Fireship'}>YouTube</Link>
-              </li>{' '}
-              |
-              <li>
-                <Link href={'/login'}>Source Code</Link>
-              </li>{' '}
-              |
-              <li>
-                <Link href={'https://nextjs.org'}>NextJS Docs</Link>
-              </li>
-            </ul>
-          </footer>
-        </div>
-      </body>
-    </html>
-    // </AuthProvider>
+            <footer>
+              <p>
+                Created for the{' '}
+                <Link href='https://fireship.io'>
+                  Fireship Next.js 13 Full Course
+                </Link>
+              </p>
+              <ul>
+                <li>
+                  <Link href={'/about'}>About</Link>
+                </li>{' '}
+                |
+                <li>
+                  <Link href={'https://www.youtube.com/@Fireship'}>
+                    YouTube
+                  </Link>
+                </li>{' '}
+                |
+                <li>
+                  <Link href={'/login'}>Source Code</Link>
+                </li>{' '}
+                |
+                <li>
+                  <Link href={'https://nextjs.org'}>NextJS Docs</Link>
+                </li>
+              </ul>
+            </footer>
+          </div>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
