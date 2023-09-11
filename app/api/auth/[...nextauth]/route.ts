7cf25e3c5d4db312124cfae24c45a5b9dd39cc51
yannickrocks <yannickmermet@hotmail.com>
2023-09-11 17:32:08 +0100

    finished nextjs course

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,18 @@
 import NextAuth from 'next-auth/next';
-import { authOptions } from '@/app/utils/authOptions';
+import { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
